26df9a564392d2c58c28b93184053f1ff341be64
Luis Isaac Cuautle <cuautle.tron@gmail.com>
2023-01-04 20:25:21 -0600

    new file:   models/Regalo.php modified:   models/Registro.php modified:   package-lock.json modified:   package.json modified:   public/build/css/app.css modified:   public/build/css/app.css.map modified:   public/build/img/speakers/2a41a781d8ae8f0f7a1969c766276b08.webp modified:   public/build/img/speakers/40e01f5c023c7e74c9c372a8126edd97.webp modified:   public/build/img/speakers/414ffd61380bbf0e9f45cbde4d0cbb7f.webp modified:   public/build/img/speakers/5332118b8d7690a1b992431802eafab1.webp modified:   public/build/img/speakers/55c7866df31370ec3299eed6eb63daa1.webp modified:   public/build/img/speakers/6497c66bcc464e26871c046dd5bb86c8.webp modified:   public/build/js/main.js.min.map modified:   public/build/js/main.min.js modified:   public/index.php modified:   src/js/registro.js modified:   src/scss/base/_globales.scss modified:   src/scss/paginas/inicio/_speakers.scss modified:   src/scss/registro/_registro.scss modified:   src/scss/ui/formulario/_formulario.scss modified:   views/registro/conferencias.php

--- src/js/registro.js
+++ src/js/registro.js
@@ -1,15 +1,116 @@
+ import Swal from "sweetalert2";
+
  (function() {
     let eventos = [];
-    const eventosBoton = document.querySelectorAll('.evento__agregar');
-    eventosBoton.forEach(boton => boton.addEventListener('click',selecionarEvento));
+    const resumen = document.querySelector('#registro-resumen');
+    if(resumen) {
 
-    function selecionarEvento({target}) {
-        // Deshabilitar el evento
-        e.target.disabled = true;
-        eventos =[...eventos, {
-            id: target.dataset.id,
-            titulo: target.parentElement.querySelector('.evento__nombre').textContent.trim()
-        }];
-        console.log(eventos);
+        const eventosBoton = document.querySelectorAll('.evento__agregar');
+        eventosBoton.forEach(boton => boton.addEventListener('click', selecionarEvento));
+
+        const formularioRegistro = document.querySelector('#registro');
+        formularioRegistro.addEventListener('submit',submitFormulario);
+
+        mostrarEventos();
+
+        function selecionarEvento({target}) {
+            
+            if(eventos.length < 5) {
+                // Deshabilitar el evento
+                target.disabled = true;
+                eventos =[...eventos, {
+                    id: target.dataset.id,
+                    titulo: target.parentElement.querySelector('.evento__nombre').textContent.trim()
+                }];
+                mostrarEventos();
+            } else {
+                Swal.fire({
+                    title: 'Error',
+                    text: 'Maximo 5 Eventos por Registro',
+                    icon: 'error',
+                    confirmButtonText: 'OK'
+                })
+            }
+
+        }
+
+        function mostrarEventos() {
+
+            // Limpiar el html
+            LimpiarEventos();
+
+            if(eventos.length > 0) {
+                eventos.forEach(evento => {
+                    const eventoDoom = document.createElement('DIV');
+                    eventoDoom.classList.add('registro__evento');
+                    
+                    const titulo = document.createElement('H3');
+                    titulo.classList.add('registro__nombre');
+                    titulo.textContent = evento.titulo;
+
+                    const botonEliminar = document.createElement('BUTTON');
+                    botonEliminar.classList.add('registro__eliminar');
+                    botonEliminar.innerHTML = '<i class="fa-solid fa-trash"></i>'
+                    botonEliminar.onclick = function () {
+                        eliminarEvento(evento.id);
+                    }
+
+                    // Renderizar en el html
+                    eventoDoom.appendChild(titulo);
+                    eventoDoom.appendChild(botonEliminar);
+                    resumen.appendChild(eventoDoom);
+                })
+            } else {
+                const noRegistros = document.createElement('P')
+                noRegistros.textContent = 'No hay eventos añade hasta 5 de lado izquierdo';
+                noRegistros.classList.add('registro__texto');
+                resumen.appendChild(noRegistros);
+            }
+        }
+
+        function LimpiarEventos() {
+            while (resumen.firstChild) {
+                resumen.removeChild(resumen.firstChild);
+            }
+        }
+
+        function eliminarEvento(id) {
+            eventos = eventos.filter(evento => evento.id != id);
+            const botonAgregar = document.querySelector(`[data-id="${id}"]`);
+            botonAgregar.disabled = false;
+            mostrarEventos();
+        }
+
+         async function submitFormulario(e) {
+            e.preventDefault();
+            
+            // Obtener el regalo
+            const regaloId = document.querySelector('#regalo').value;
+            const eventosId = eventos.map(evento => evento.id);
+
+            if(eventosId.length === 0 || regaloId === '' ) {
+                Swal.fire({
+                    title: 'Error',
+                    text: 'Debes seleccionar por lo menos un evento y un regalo',
+                    icon: 'error',
+                    confirmButtonText: 'OK'
+                });
+                return;
+            }
+
+            // Objeto de formdata
+            const datos = new FormData() ;
+            datos.append('eventos', eventosId);
+            datos.append('regalo_id', regaloId);
+
+            const url = '/finalizar-registro/conferencias';
+            const respuesta =  await fetch(url, {
+                method: 'POST',
+                body: datos
+            })
+            
+            const resultado = await respuesta.json()
+            console.log(resultado);
+        }
     }
  })();
